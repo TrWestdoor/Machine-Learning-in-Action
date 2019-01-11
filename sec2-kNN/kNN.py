1e965d4a196e4b0c3f72ef81bde2d794b536bd61
Shiwei-Song <shiweisongssw@foxmail.com>
2019-01-11 21:51:07 +0800

    sec2 done

--- sec2-kNN/kNN.py
+++ sec2-kNN/kNN.py
@@ -1,17 +1,31 @@
-from numpy import *
+import numpy as np
 import operator
 import matplotlib
 import matplotlib.pyplot as plt
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
 
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVlas = dataSet.max(0)
     ranges = maxVlas - minVals
-    normDataSet = zeros(shape(dataSet))
     m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals, (m,1))
-    normDataSet = normDataSet / tile(ranges, (m,1))
+    normDataSet = dataSet - np.tile(minVals, (m,1))
+    normDataSet = normDataSet / np.tile(ranges, (m,1))
     return normDataSet, ranges, minVals
 
 
@@ -20,7 +34,7 @@ def file2matrix(filename):
     # 此处书中为 fr.readline(),根据上下文代码判断，应当为 fr.readlines()函数才正确。
     arrayOLines = fr.readlines()
     numberOfLines = len(arrayOLines)
-    returnMat = zeros((numberOfLines, 3))
+    returnMat = np.zeros((numberOfLines, 3))
     classLabelVector = []
     index = 0
     for line in arrayOLines:
@@ -32,9 +46,42 @@ def file2matrix(filename):
     return returnMat, classLabelVector
 
 
-datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
-normMat, ranges, minVals = autoNorm(datingDataMat)
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15.0*array(datingLabels), 15.0 * array(datingLabels))
-plt.show()
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCreame = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix("datingTestSet2.txt")
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = np.array([ffMiles, percentTats, iceCreame])
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+    print("You will probably like this person: ", resultList[classifierResult - 1])
+
+
+def main():
+    classifyPerson()
+    """
+    percentage of time spent playing video games?10
+    frequent flier miles earned per year?10000
+    liters of ice cream consumed per year?0.5
+    You will probably like this person:  in small doses
+    """
+
+
+if __name__ == '__main__':
+    main()
---
+++ sec2-kNN/numberClassify.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+import kNN
+
+
+def img2vector(filename):
+    returnVect = np.zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = kNN.classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if classifierResult != classNumStr:
+            errorCount += 1.0
+    print("\n the total nubmer of errors is: %d" % errorCount)
+    print("\n the total error rate is: %f" % (errorCount/float(mTest)))
+
+
+handwritingClassTest()
