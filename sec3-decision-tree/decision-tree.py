3f12535f4f84e7bfd58c41893527789edb1aa1eb
Shiwei-Song <shiweisongssw@foxmail.com>
2019-01-15 17:39:51 +0800

    add sec3

---
+++ sec3-decision-tree/decision-tree.py
@@ -0,0 +1,37 @@
+from math import log
+
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def main():
+    myDat, labels = createDataSet()
+    myDat[0][-1] = 'maybe'
+    print(myDat)
+    print(calcShannonEnt(myDat))
+
+
+if __name__ == '__main__':
+    main()
