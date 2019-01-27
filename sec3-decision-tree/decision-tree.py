c8245560b251bcbaa7b45311f862e2238384198b
Shiwei-Song <shiweisongssw@foxmail.com>
2019-01-27 22:29:10 +0800

    update

--- sec3-decision-tree/decision-tree.py
+++ sec3-decision-tree/decision-tree.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 
 def calcShannonEnt(dataSet):
@@ -54,6 +55,24 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt()
 
 
 def main():
