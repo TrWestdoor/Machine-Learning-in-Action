e3fc4ba7ad3e7f12aef8c5b0805d05f83a266ce7
Shiwei-Song <shiweisongssw@foxmail.com>
2019-03-24 23:56:55 +0800

    init LR

---
+++ sec5-LR/logRegres.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def loadDataSet():
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+
+def sigmoid(inX):
+    return 1.0 / (1+np.exp(-inX))
+
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = np.mat(dataMatIn)
+    labelMat = np.mat(classLabels).transpose()
+    m, n = np.shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = np.ones((n, 1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix * weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.transpose() * error
+    return weights
+
+
+def main():
+    dataArr, labelMat = loadDataSet()
+    print(gradAscent(dataArr, labelMat))
+
+
+if __name__ == '__main__':
+    main()
