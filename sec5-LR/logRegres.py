572a94bb6ba80ee32adbf5e7a9a4b23043442178
Shiwei-Song <shiweisongssw@foxmail.com>
2019-03-25 00:09:03 +0800

    updata

--- sec5-LR/logRegres.py
+++ sec5-LR/logRegres.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def loadDataSet():
@@ -30,9 +31,38 @@ def gradAscent(dataMatIn, classLabels):
     return weights
 
 
+def plotBestFit(weights):
+    dataMat, labelMat = loadDataSet()
+    dataArr = np.array(dataMat)
+    n = np.shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
 def main():
     dataArr, labelMat = loadDataSet()
-    print(gradAscent(dataArr, labelMat))
+    # print(gradAscent(dataArr, labelMat))
+    weights = gradAscent(dataArr, labelMat)
+    plotBestFit(weights.getA())
 
 
 if __name__ == '__main__':
