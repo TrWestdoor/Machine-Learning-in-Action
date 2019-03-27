32a24356f3698c4ace7ae170befcf8ffce9c6f8e
Shiwei-Song <shiweisongssw@foxmail.com>
2019-03-27 23:47:52 +0800

    done LR

--- sec5-LR/logRegres.py
+++ sec5-LR/logRegres.py
@@ -84,6 +84,45 @@ def plotBestFit(weights):
     plt.show()
 
 
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX*weights))
+    if prob>0.5: return 1.0
+    else: return 0.0
+
+
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = []; trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(np.array(trainingSet), trainingLabels, 500)
+    errorCount = 0; numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(np.array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print("the error rate of this test is: %f " % errorRate)
+    return errorRate
+
+
+def multiTest():
+    numTests = 10; errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f " % (numTests, errorSum/float(numTests)))
+
+
 def main():
     dataArr, labelMat = loadDataSet()
     '''5-1
@@ -95,10 +134,14 @@ def main():
     weights = stocGradAcent0(np.array(dataArr), labelMat)
     plotBestFit(weights)
     '''
+    '''
     # 5-4
     weights = stocGradAscent1(np.array(dataArr), labelMat)
     plotBestFit(weights)
-
+    '''
+    # 5-5
+    multiTest()
+    
 
 if __name__ == '__main__':
     main()
