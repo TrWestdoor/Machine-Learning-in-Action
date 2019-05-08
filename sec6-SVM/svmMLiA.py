473d118c99bf2fde4ad27fc2cfddf7328e66938c
Shiwei-Song <shiweisongssw@foxmail.com>
2019-05-08 13:52:06 +0800

    done SVM

--- sec6-SVM/svmMLiA.py
+++ sec6-SVM/svmMLiA.py
@@ -226,12 +226,12 @@ def kernelTracs(X, A, kTup):
     return K
 
 
-def testRbf(k1 = 1.3):
+def testRbf(k1=1.3):
     dataArr, labelArr = loadDataSet('testSetRBF.txt')
     b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1))
     datMat = np.mat(dataArr)
     labelMat = np.mat(labelArr).transpose()
-    svInd = np.nonzero(alphas.A>0)[0]
+    svInd = np.nonzero(alphas.A > 0)[0]
     sVs = datMat[svInd]
     labelSV = labelMat[svInd]
     print("there are %d Support Vectors" % np.shape(sVs)[0])
@@ -245,15 +245,73 @@ def testRbf(k1 = 1.3):
     print("the training error rate is: %f" % (float(errorCount)/m))
     dataArr, labelArr = loadDataSet('testSetRBF2.txt')
     errorCount = 0
-    dataMat = np.mat(dataArr)
+    datMat = np.mat(dataArr)
     labelMat = np.mat(labelArr).transpose()
     m, n = np.shape(datMat)
     for i in range(m):
-        kernelEval = kernelTracs(sVs, datMat[i, :], ('rfb', k1))
+        kernelEval = kernelTracs(sVs, datMat[i, :], ('rbf', k1))
         predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
         if np.sign(predict) != np.sign(labelArr[i]):
             errorCount += 1
-        print("the test error rate is: %f " % (float(errorCount)/m))
+    print("the test error rate is: %f " % (float(errorCount)/m))
+
+
+def img2vector(filename):
+    returnVect = np.zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+
+
+def loadImages(dirName):
+    from os import listdir
+    hwLabels = []
+    trainingFileList = listdir(dirName)
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        if classNumStr == 9:
+            hwLabels.append(-1)
+        else:
+            hwLabels.append(1)
+        trainingMat[i, :] = img2vector('%s/%s' % (dirName, fileNameStr))
+    return  trainingMat, hwLabels
+
+
+def testDigits(kTup=('rbf', 10)):
+    dataArr, labelArr = loadImages('trainingDigits')
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, kTup)
+    datMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    svInd = np.nonzero(alphas.A>0)[0]
+    sVs = datMat[svInd]
+    labelSV = labelMat[svInd]
+    print("there are %d Support Vectors" % np.shape(sVs)[0])
+    m, n = np.shape(datMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTracs(sVs, datMat[i, :], kTup)
+        predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+        if np.sign(predict) != np.sign(labelArr[i]):
+            errorCount += 1
+    print("the training error rate is: %f " % (float(errorCount)/m))
+    dataArr, labelArr = loadImages('testDigits')
+    errorCount = 0
+    datMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    m, n = np.shape(datMat)
+    for i in range(m):
+        kernelEval = kernelTracs(sVs, datMat[i, :], kTup)
+        predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+        if np.sign(predict) != np.sign(labelArr[i]):
+            errorCount += 1
+    print("the test error rate is: %f" % (float(errorCount)/m))
 
 
 def main():
@@ -268,9 +326,13 @@ def main():
         if alphas[i]>0.0:
             print(dataArr[i], labelArr[i])
     '''
+    '''
     b, alphas = smoP(dataArr, labelArr, 0.6, 0.001, 40)
     ws = calcWs(alphas, dataArr, labelArr)
     print(ws)
+    '''
+    # testRbf()
+    testDigits(('rbf', 20))
 
 
 if __name__ == '__main__':
