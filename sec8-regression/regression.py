2f45bb15e08760078d925dc7705670d5308d0ac3
TrWestdoor <shiweisongssw@foxmail.com>
2019-05-16 22:01:29 +0800

    lwlr

--- sec8-regression/regression.py
+++ sec8-regression/regression.py
@@ -28,14 +28,41 @@ def standRegres(xArr, yArr):
     return ws
 
 
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T
+    m = np.shape(xMat)[0]
+    weights = np.mat(np.eye((m)))
+    for j in range(m):
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = np.exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+    xTx = xMat.T * (weights * xMat)
+    if np.linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = np.shape(testArr)[0]
+    yHat = np.zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
+
 def main():
     xArr, yArr = loadDataSet('ex0.txt')
     # print(xArr[0:2])
+    '''
     ws = standRegres(xArr, yArr)
     # print(ws)
     xMat = np.mat(xArr)
     yMat = np.mat(yArr)
     yHat = xMat * ws
+    '''
     '''
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -46,7 +73,16 @@ def main():
     ax.plot(xCopy[:, 1], yHat)
     plt.show()
     '''
-    print(np.corrcoef(yHat.T, yMat))
+    # print(np.corrcoef(yHat.T, yMat))
+    yHat = lwlrTest(xArr, xArr, yArr, 0.01)
+    xMat = np.mat(xArr)
+    srtInd = xMat[:, 1].argsort(0)
+    xSort = xMat[srtInd][:, 0, :]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(xSort[:, 1], yHat[srtInd])
+    ax.scatter(xMat[:, 1].flatten().A[0], np.mat(yArr).T.flatten().A[0], s=2, c='red')
+    plt.show()
 
 
 if __name__ == '__main__':
