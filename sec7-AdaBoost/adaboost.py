ce7d31b69302457557060a480b0c06b28a3d536b
TrWestdoor <374251766@qq.com>
2019-08-06 17:50:43 +0800

    update sec7

--- sec7-AdaBoost/adaboost.py
+++ sec7-AdaBoost/adaboost.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def loadSimpData():
@@ -9,6 +10,26 @@ def loadSimpData():
                      [2. , 1. ]])
     classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
     return datMat, classLabels
+
+
+def stumpClassify(dataMatrix, dimen, threashVal, threshIneq):
+    retArray = np.ones((np.shape(dataMatrix)[0], 1))
+    if threshIneq == 'lt':
+        retArray[dataMatrix[:, dimen] <= threashVal] = -1.0
+    else:
+        retArray[dataMatrix[:, dimen] > threashVal] = -1.0
+    return retArray
+
+
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = np.mat(dataArr)
+    labelMat = np.mat(classLabels).T
+    m, n = np.shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    bestClasEst = np.mat(np.zeros((m, 1)))
+    minError = np.inf
+    
 
 
 def main():
