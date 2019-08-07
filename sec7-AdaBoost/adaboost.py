f5ea59e2dab28a2a1683d85fe86873071b845bf3
TrWestdoor <374251766@qq.com>
2019-08-07 17:51:25 +0800

    update adaboost

--- sec7-AdaBoost/adaboost.py
+++ sec7-AdaBoost/adaboost.py
@@ -29,11 +29,68 @@ def buildStump(dataArr, classLabels, D):
     bestStump = {}
     bestClasEst = np.mat(np.zeros((m, 1)))
     minError = np.inf
-    
+    # Recurrent each dimension.
+    for i in range(n):
+        rangeMin = dataMatrix[:, i].min()
+        rangeMax = dataMatrix[:, i].max()
+        stepSize = (rangeMax - rangeMin) / numSteps
+
+        # Recurrent all value according stepSize in this dimension.
+        for j in range(-1, int(numSteps)+1):
+            # Two judge way.
+            for inequal in ['lt', 'gt']:
+                threshVal = (rangeMin + float(j) * stepSize)
+                predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+                errArr = np.mat(np.ones((m, 1)))
+                errArr[predictedVals == labelMat] = 0
+                weightedError = D.T * errArr
+                # print("split: dim %d, thresh %.2f, thresh inequal: %s, the weighted error is %.3f" % \
+                #       (i, threshVal, inequal, weightedError))
+                if weightedError < minError:
+                    minError = weightedError
+                    bestClasEst = predictedVals.copy()
+                    bestStump['dim'] = i
+                    bestStump['thresh'] = threshVal
+                    bestStump['ineq'] = inequal
+    return bestStump, minError, bestClasEst
+
+
+def adaBoostTrainDS(dataArr, classLables, numIt=40):
+    weakClassArr = []
+    m = np.shape(dataArr)[0]
+    D = np.mat(np.ones((m, 1)) / m)
+    # Record class aggregate value in each data point.
+    aggClassEst = np.mat(np.zeros((m, 1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLables, D)
+        print("D: ", D.T)
+        alpha = float(0.5*np.log((1.0 - error) / max(error, 1e-16)))
+        bestStump['alhpa'] = alpha
+        weakClassArr.append(bestStump)
+        print("classEst: ", classEst.T)
+        # which has some question need to study!!!!!!!!!!!!!! as end to end.
+        expon = np.multiply(-1*alpha*np.mat(classLables).T, classEst)
+        D = np.multiply(D, np.exp(expon))
+        D = D/D.sum()
+        aggClassEst += alpha*classEst
+        print("aggClassEst: ", aggClassEst.T)
+        aggErrors = np.multiply(np.sign(aggClassEst) != np.mat(classLables).T, np.ones((m, 1)))
+        errorRate = aggErrors.sum() / m
+        print("total error: ", errorRate, "\n")
+        if errorRate == 0.0:
+            break
+    return weakClassArr
 
 
 def main():
     datMat, classLabels = loadSimpData()
+    # In p121
+    # D = np.mat(np.ones((5, 1)) / 5)
+    # print(buildStump(datMat, classLabels, D))
+    ###################
+
+    # programming 7-2
+    classifierArray = adaBoostTrainDS(datMat, classLabels, 9)
 
 
 if __name__ == '__main__':
